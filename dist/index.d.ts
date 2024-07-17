21c25b1f088848ab1e8594b6378d5c73c4fb1b6a
micaiguai <1399880823@qq.com>
2024-07-17 11:05:35 +0800

    chore: update package.json

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -10,6 +10,7 @@ type GetItem<L extends readonly Item[], V extends unknown> = L extends readonly 
 type EnumInfo<T extends readonly Item[], E = {}> = T extends readonly [infer F extends Item, ...infer R extends Item[]] ? EnumInfo<R, MergeInsertions<E & {
     [K in F['key']]: F['value'];
 }>> : E;
+type EnumValue<E extends {}> = MergeInsertions<E> extends Record<string, infer T> ? T : never;
 declare function enumify<T extends readonly Item[]>(items: T): EnumInfo<T> & (<V>(value: V) => GetItem<T, V>);
 
-export { enumify };
+export { type EnumValue, enumify };
