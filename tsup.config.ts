13336cc39413553d6fa16835c3d003d1564b53fd
micaiguai <1399880823@qq.com>
2024-07-18 14:00:17 +0800

    chore: update package.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: 'esm',
+  minify: true,
   splitting: false,
   sourcemap: false,
   clean: true,
